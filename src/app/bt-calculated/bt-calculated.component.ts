065bc04b3a565d24f50956475d1057ac180cfb11
berend.wilkens <berend.wilkens@C1405260.medewerkers.ad.hvu.nl>
2018-03-05 11:38:21 +0100

    kleine wijziging

--- src/app/bt-calculated/bt-calculated.component.ts
+++ src/app/bt-calculated/bt-calculated.component.ts
@@ -1,31 +1,33 @@
 import {Component, ElementRef, EventEmitter, Input, OnInit, Output, ViewChild, Directive} from '@angular/core';
 
 @Component({
-	selector: 'bt-calculated',
-	templateUrl: 'bt-calculated.component.html',
+   selector: 'bt-calculated',
+   templateUrl: 'bt-calculated.component.html',
 })
 export class BtCalculatedComponent {
-	private _profiel: Array<any>;
-	matrix: Array<any>;
-	 
-	@Input()
-	set profiel(profiel: Array<any>) {
-		console.log("SET this._profiel");
-		this._profiel = profiel;
-		console.log(this._profiel);
-		this.matrix = this.generateMatrix("-");
-		if (this._profiel != null){ 
-			for (let bt of this._profiel) {
-				this.matrix[bt.beroepsTaak.architectuurlaagId][bt.beroepsTaak.activiteitId] = bt;
-			}
-		}
-	}
+   private _profiel: Array<any>;
+   matrix: Array<any>;
 
-	get profiel(): Array<any> {return this._profiel;}
-	
+   @Input()
+   set profiel(profiel: Array<any>) {
+      console.log('SET this._profiel');
+      this._profiel = profiel;
+      console.log(this._profiel);
+      this.matrix = this.generateMatrix('-');
+      if (this._profiel != null) {
+         for (let bt of this._profiel) {
+            this.matrix[bt.beroepsTaak.architectuurlaagId][bt.beroepsTaak.activiteitId] = bt;
+         }
+      }
+   }
+
+   get profiel(): Array<any> {
+      return this._profiel;
+   }
+
 	ngOnInit(): void {
-	console.log("Start of bt_calculated.component");
-		this.matrix = this.generateMatrix("x");
+	console.log('Start of bt_calculated.component');
+		this.matrix = this.generateMatrix('x');
 		if (this._profiel != null){ 
 			for (let bt of this._profiel) {
 				this.matrix[bt.beroepsTaak.architectuurlaagId][bt.beroepsTaak.activiteitId] = bt;
