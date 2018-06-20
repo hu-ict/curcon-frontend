8daa6c61b85129e9c17515d4e0d761cfc559422c
Janhub <jan.dekkers@student.hu.nl>
2018-06-20 13:17:38 +0200

    mergen met martins commit

--- src/app/ps-overzicht/ps-overzicht.module.ts
+++ src/app/ps-overzicht/ps-overzicht.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { ChartsModule } from 'ng2-charts/ng2-charts';
-import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { NgbDropdownModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { CommonModule } from '@angular/common';
 import { TablesComponent } from '../components/tables.component';
 
 // Modal Component
-import { ModalModule } from 'ngx-bootstrap/modal';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { NgbModalModule } from '@ng-bootstrap/ng-bootstrap';
 import { ModalsComponent } from '../components/modals.component';
 
 // Tabs Component
@@ -25,18 +26,18 @@ import {PsOverzichtComponent} from '../ps-overzicht/ps-overzicht.component';
 	imports: [
 		ChartsModule,
 		HttpModule,
-		BsDropdownModule,
+		NgbDropdownModule,
 		FormsModule,
 		CommonModule,
 		TabsModule,
 		TooltipModule,
-		ModalModule.forRoot()
+//		ModalModule.forRoot()
 		],
-	declarations: [ 
+	declarations: [
 		PsOverzichtComponent
 	],
-	exports: [ 
-		PsOverzichtComponent 
+	exports: [
+		PsOverzichtComponent
 	],
 	providers: [
 	]
