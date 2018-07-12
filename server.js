741aa959f7047e12451d19ac69c74e66acf49545
Redouan Bouzid <redouanbouzid@air-van-redouan.wifi.hu.nl>
2018-07-12 14:18:04 +0200

    wijzigingen voor heroku

--- server.js
+++ server.js
@@ -1,16 +1,12 @@
-//Install express server
 const express = require('express');
+const app = express();
 const path = require('path');
 
-const app = express();
+app.use(express.static(__dirname + '/dist'));
+app.listen(process.env.PORT || 8080); 
 
-// Serve only the static files form the dist directory
-app.use(express.static('/src/app/logins'));
-
-app.get('/*', function(req,res) {
-    
-res.sendFile(path.join(__dirname+'/src/app/logins/logins.component.html'));
+app.get('/*', function(req, res) {
+	res.sendFile(path.join(__dirname + '/dist/index.html'));
 });
 
-// Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+console.log('Console Listening!');
